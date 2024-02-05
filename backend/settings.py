5e48db006b74c2cfb8f3b87a26faf0a4d8235889
Alan Juárez Fragoso <alanjuarezfragoso@MacBook-Pro-de-Alan.local>
2024-02-04 21:38:58 -0600

    ya no se que hago xd

--- backend/settings.py
+++ backend/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'dist/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'dist/static')
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'dist/static')
     STATICFILES_DIRS=[BASE_DIR / "dist/static",]
