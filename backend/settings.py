4c72300cddf262231d7ad769f522d5334135ced7
Alan Juárez Fragoso <alanjuarezfragoso@MacBook-Pro-de-Alan.local>
2024-02-04 21:32:02 -0600

    prueba 5 ya no se que hago xd

--- backend/settings.py
+++ backend/settings.py
@@ -134,7 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'dist/static')
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'dist/static')
     STATICFILES_DIRS=[BASE_DIR / "dist/static",]
