94c230d705a125f2b91956f108b83867d3b70d49
Alexhr <alex.hernandez24kb@gmail.com>
2024-02-05 00:11:18 -0600

    correcion de ruta estatica

--- backend/settings.py
+++ backend/settings.py
@@ -134,16 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'dist/static')
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'dist/static')
-    STATICFILES_DIRS=[BASE_DIR / "dist/static",]
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "dist/static")
+    os.path.join(BASE_DIR, 'dist/static')
 ]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
