b866fc1be63a1b23efd12cce8d1f893e2194c27d
Alan Juarez <alaanjuarezf@gmail.com>
2024-02-10 12:59:04 -0600

    prueba sin eliminar base de datos

--- backend/settings.py
+++ backend/settings.py
@@ -135,7 +135,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dist/static')
 ]
